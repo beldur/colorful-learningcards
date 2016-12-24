f53c1756d821ea2282dcc167e38efed4f02a51c1
Beldur <alexluecking@gmail.com>
2016-12-24 19:52:41 +0100

    Fixed tests

--- src/modules/auth/reducer.test.js
+++ src/modules/auth/reducer.test.js
@@ -5,21 +5,24 @@ describe('auth/reducer', () => {
   it(`should handle ${STATE_CHANGED}`, () => {
     const user = { name: 'Tester' }
 
-    expect(reducer({}, stateChanged(user))).toEqual({
+    expect(reducer(undefined, stateChanged(user))).toEqual({
       authenticated: true,
       user,
+      initialized: true,
     })
 
-    expect(reducer({}, stateChanged(null))).toEqual({
+    expect(reducer(undefined, stateChanged(null))).toEqual({
       authenticated: false,
       user: null,
+      initialized: true,
     })
   })
 
   it(`should handle ${LOGOUT_SUCCESS}`, () => {
-    expect(reducer({}, logoutSuccess())).toEqual({
+    expect(reducer(undefined, logoutSuccess())).toEqual({
       authenticated: false,
-      user: null
+      user: null,
+      initialized: false,
     })
   })
 })
